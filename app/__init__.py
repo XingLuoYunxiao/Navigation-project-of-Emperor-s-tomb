35b75ce9b3584b353a5b3dece97ff1270d2a4046
XingLuoYunxiao <zy3139565068@gmail.com>
2024-06-20 23:15:13 +0800

    登陆 注册的优化

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# 设置一个随机生成的secret_key
+app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Tang18Ling:1751246+-k@101.200.79.159/tang18ling'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app, db
 from app.models import User
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -12,10 +12,16 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        
+        if password != confirm_password:
+            flash('两次密码输入不匹配，请重试')
+            return redirect(url_for('register'))
+            
         new_user = User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -25,5 +31,11 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(email=email, password=password).first()
         if user:
-            return redirect(url_for('index'))
+            return render_template('login.html', message="登录成功，三秒后跳转到主页")
+        else:
+            flash('登录失败，请检查邮箱和密码')
     return render_template('login.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
